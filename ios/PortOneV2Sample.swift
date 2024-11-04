86724c1747b209d0c3c59c37785c5935227288e6
Jaemin Yu (Holden) <48317457+youjmen@users.noreply.github.com>
2024-11-04 12:29:42 +0900

    feat: ios sample 추가 (#5)

---
+++ ios/PortOneV2Sample.swift
@@ -0,0 +1,17 @@
+//
+//  KpnTestApp.swift
+//  KpnTest
+//
+//  Created by Holden on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PortOneV2Sample: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
